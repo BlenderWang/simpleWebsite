1e548f3daf1f41a438c85b1e64d565d0cd259183
Shirley Wang <shirleywang527@gmail.com>
2019-08-23 13:00:59 +0200

    smooth scroll with js library

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
+// Smooth Scroll
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 800
+});
+
 // debounce func
-function debounce(func, wait = 20, immediate = true) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
+// function debounce(func, wait = 20, immediate = true) {
+// 	var timeout;
+// 	return function() {
+// 		var context = this, args = arguments;
+// 		var later = function() {
+// 			timeout = null;
+// 			if (!immediate) func.apply(context, args);
+// 		};
+// 		var callNow = immediate && !timeout;
+// 		clearTimeout(timeout);
+// 		timeout = setTimeout(later, wait);
+// 		if (callNow) func.apply(context, args);
+// 	};
+// };
 
-const slideImages = document.querySelectorAll('.slide-in')
+// const slideImages = document.querySelectorAll('.slide-in')
 
-function checkSlide(e) {
-    slideImages.forEach(slideImage => {
-        // scroll to halfway through the page
-        const slideInAt =(window.scrollY + window.innerHeight) - slideImage.height / 2
-        // bottom of the image
-        const imageBottom = slideImage.offsetTop + slideImage.height
+// function checkSlide(e) {
+//     slideImages.forEach(slideImage => {
+//         // scroll to halfway through the page
+//         const slideInAt =(window.scrollY + window.innerHeight) - slideImage.height / 2
+//         // bottom of the image
+//         const imageBottom = slideImage.offsetTop + slideImage.height
 
-        const isHalfShown = slideInAt > slideImage.offsetTop
-        //if the image is scrolled passed or not
-        const isNotScrolledPast = window.scrollY < imageBottom
+//         const isHalfShown = slideInAt > slideImage.offsetTop
+//         //if the image is scrolled passed or not
+//         const isNotScrolledPast = window.scrollY < imageBottom
 
-        if(isHalfShown && isNotScrolledPast){
-            slideImage.classList.add('active');
-        } else {
-            slideImage.classList.remove('active');
-        }
-    })
-}
+//         if(isHalfShown && isNotScrolledPast){
+//             slideImage.classList.add('active');
+//         } else {
+//             slideImage.classList.remove('active');
+//         }
+//     })
+// }
 
-window.addEventListener('scroll', debounce(checkSlide))
+// window.addEventListener('scroll', debounce(checkSlide))
 
 // show all partners
 const showMoreBtn = document.querySelector('.btn-show-more')
